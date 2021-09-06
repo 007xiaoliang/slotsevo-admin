c075a1df58617ad684296f52f46a3168ca3207ff
xiaoliang <xuliang@lettucegames.com>
2021-09-06 17:27:26 +0800

    goroutine

--- server/rpc/httpCient_test.go
+++ server/rpc/httpCient_test.go
@@ -2,14 +2,10 @@ package rpc
 
 import (
 	"fmt"
-	"slotsevo-admin/core"
-	"slotsevo-admin/global"
 	"testing"
 )
 
 func TestRequestHttp(t *testing.T) {
-	global.SlotsVp = core.Viper("../config.yaml")
-	global.TraceLog, global.AccessLog, global.BaseSendLog, global.ErrorLog = core.Zap()
 	var activityTypeArr []string
 	err := RequestHttp("POST", "debug", map[string]interface{}{
 		"cmd": "get_debug_themes",
@@ -18,4 +14,15 @@ func TestRequestHttp(t *testing.T) {
 		fmt.Println(err)
 	}
 	fmt.Println(activityTypeArr)
+}
+
+func TestRequestHttpWithDetail(t *testing.T) {
+	//var rpcActivityType string
+	resp, err := RequestHttpWithDetail("POST", "debug", "192.168.2.34", "7001", map[string]interface{}{
+		"cmd": "get_server_time",
+	}, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(resp.String())
 }
